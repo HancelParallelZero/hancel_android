f5efcdddef78ce642b5dc55eae7d247d5e96ae3b
Antonio Vanegas <hpsaturn@gmail.com>
2015-11-05 23:55:06 -0500

    add Ring Adapter mockup

---
+++ app/src/main/java/org/parallelzero/hancel/models/Ring.java
@@ -0,0 +1,17 @@
+package org.parallelzero.hancel.models;
+
+import java.util.List;
+
+/**
+ * Created by Antonio Vanegas @hpsaturn on 11/5/15.
+ */
+public class Ring {
+
+    public String name;
+
+    public List<Contact> contacts;
+
+    public String getName() {
+        return name;
+    }
+}
